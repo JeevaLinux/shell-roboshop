812942b48790f5472465a4e2215456c99add0900
Jeeva <endlurijeeva93@mail.com>
2025-10-14 16:03:34 +0530

    session-06

--- mongodb.sh
+++ mongodb.sh
@@ -44,5 +44,5 @@ VALIDATE $? "Start MongoDB"
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
 VALIDATE $? "Allowing remote connections to MongoDB"
 
-systemctl retsrat mongod
+systemctl restart mongod
 VALIDATE $? "Restarted MongoDB"
